e8a30303960fee7d99277a830a9c46a8a21c485c
Cody Maggard <cody.maggard@gmail.com>
2010-04-15 17:43:33 -0500

    Few tests, few codes. no big.

--- lib/trends/trend_analyzer.rb
+++ lib/trends/trend_analyzer.rb
@@ -4,10 +4,23 @@ module Trends
       @trend_dict = Hash.new(0)
     end
 
-    def add(tweet)
-      @trend_dict[tweet] = @trend_dict[tweet] + 1
+    def process(tweet)
+      temp_hash = {}
+      tweet.split.each do |w|
+        res = add(w)
+        temp_hash[res] = true unless res.nil?
+      end
+      temp_hash.keys.each do |k|
+        @trend_dict[k] = @trend_dict[k] + 1
+      end
     end
 
+    private
+    def add(word)
+      return if word[0] == '@'
+      w = word.gsub(/[^a-zA-Z0-9#]/, '')
+      #@trend_dict[w] = @trend_dict[w] + 1
+    end
   end
 end
 
--- test/test_trends.rb
+++ test/test_trends.rb
@@ -12,15 +12,31 @@ class TestTrends < Test::Unit::TestCase
       @trend = Trends::TrendAnalyzer.new
     end
 
-    context "and its trend terms" do
-      should "start empty" do
-        assert_empty @trend.trend_dict
-      end
+    should "start empty" do
+      assert_empty @trend.trend_dict
+    end
 
+    context "when processing" do
       should "only contain 'derp' for a tweet that says 'derp'" do
-        @trend.add("derp")
+        @trend.process("derp")
         assert_equal ['derp'], @trend.trend_dict.keys
       end
+      
+      should "ignore @ replies" do
+        @trend.process("@cmaggard derp")
+        assert_equal ['derp'], @trend.trend_dict.keys
+      end
+
+      should "ignore punctuation" do
+        @trend.process("derp,")
+        assert_equal ['derp'], @trend.trend_dict.keys
+      end
+
+      should "not count a word more than once in the same tweet" do
+        @trend.process("derp derp a derp derp derp")
+        assert_equal 1, @trend.trend_dict['derp']
+      end
     end
+
   end
 end
